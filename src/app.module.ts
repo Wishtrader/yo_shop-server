1d6978d5ce34f1215486da4d1847ee71b3a082e6
Andrei Kamarou <mr.akomarov@gmail.com>
2023-04-20 01:20:23 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import {Module} from '@nestjs/common';
+import {UsersModule} from './users/users.module';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {ConfigModule} from "@nestjs/config";
+import {SequelizeConfigService} from "./config/sequelizeConfig.service";
+import {databaseConfig} from "./config/configuration";
+
+@Module({
+  imports: [
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: SequelizeConfigService
+    }),
+    ConfigModule.forRoot({
+      load: [databaseConfig],
+    }),
+    UsersModule],
+  controllers: [],
+  providers: [],
+})
+
+export class AppModule {
+}
