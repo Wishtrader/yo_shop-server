8a40953eb3f94ac10dfab13d104b0bb415858ccd
Andrei Kamarou <mr.akomarov@gmail.com>
2023-04-25 02:52:46 +0300

    Boiler parts api and swagger doc completed

--- seeders/20230424084907-boiler-parts.js
+++ seeders/20230424084907-boiler-parts.js
@@ -1,35 +1,35 @@
-const { faker } = require('@faker-js/faker');
-('use strict');
+const { faker } = require("@faker-js/faker");
+("use strict");
 
 const boilerManufacturers = [
-  'Ariston',
-  'Chaffoteaux&Maury',
-  'Baxi',
-  'Bongioanni',
-  'Saunier Duval',
-  'Buderus',
-  'Strategist',
-  'Henry',
-  'Northwest',
+  "Ariston",
+  "Chaffoteaux&Maury",
+  "Baxi",
+  "Bongioanni",
+  "Saunier Duval",
+  "Buderus",
+  "Strategist",
+  "Henry",
+  "Northwest"
 ];
 const partsManufacturers = [
-  'Azure',
-  'Gloves',
-  'Cambridgeshire',
-  'Salmon',
-  'Montana',
-  'Sensor',
-  'Lesly',
-  'Radian',
-  'Gasoline',
-  'Croatia',
+  "Azure",
+  "Gloves",
+  "Cambridgeshire",
+  "Salmon",
+  "Montana",
+  "Sensor",
+  "Lesly",
+  "Radian",
+  "Gasoline",
+  "Croatia"
 ];
 
-/** @type {import('sequelize-cli').Migration} */
+/** @type {import("sequelize-cli").Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert(
-      'BoilerParts',
+      "BoilerParts",
       [...Array(100)].map(() => ({
         boiler_manufacturer:
           boilerManufacturers[
@@ -45,8 +45,8 @@ module.exports = {
         images: JSON.stringify(
           [...Array(7)].map(
             () =>
-              `${faker.image.technics()}?random=${faker.random.numeric(30)}`,
-          ),
+              `${faker.image.technics()}?random=${faker.random.numeric(30)}`
+          )
         ),
         vendor_code: faker.internet.password(),
         in_stock: faker.random.numeric(1),
@@ -55,12 +55,12 @@ module.exports = {
         popularity: faker.random.numeric(3),
         compatibility: faker.lorem.sentence(7),
         createdAt: new Date(),
-        updatedAt: new Date(),
-      })),
+        updatedAt: new Date()
+      }))
     );
   },
 
   async down(queryInterface, Sequelize) {
-    return queryInterface.bulkDelete('BoilerParts', null, {});
-  },
+    return queryInterface.bulkDelete("BoilerParts", null, {});
+  }
 };
